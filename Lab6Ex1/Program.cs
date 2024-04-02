da0bdaa03a763ddd0310d3cabbbc7e2d4471cc17
Administrator <Administrator@DESKTOP-IKD2D8I>
2024-04-02 18:58:58 +0300

    Add project files.

---
+++ Lab6Ex1/Program.cs
@@ -0,0 +1,44 @@
+﻿/*• Ex 1
+Un dulap are trei caracteristici : lungime, latime, inaltime. Dulapul va avea o
+metoda care va calcula volumul acestuia.
+Folositi o clasa care va modela un dulap.
+ Clasa va avea trei campuri private intregi: lungime, latime, inaltime.
+ Cele trei campuri vor fi initializate cu ajutorul constructorului
+ Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri.
+Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si
+afisati rezultatul.*/
+
+Closet closet = new(10, 5, 2);
+
+int closetVolume = closet.volume();
+Console.WriteLine("The volume of the closet is: " + closetVolume);
+
+
+class Closet
+{
+    private int length;
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// Enter values for volume calculations
+    /// </summary>
+    /// <param name="closet-width"></param>
+    /// <param name="closet-height"></param>
+    /// <param name="closet-length"></param>
+    public Closet(int closetwidth, int closetheight, int closetlength)
+    {
+        this.width = closetwidth;
+        this.height = closetheight;
+        this.length = closetlength;
+        
+    }
+
+    public int volume()
+    {
+        int volume = width* height * length;
+        return volume;  
+    }
+            
+
+}
---
+++ Lab6Ex2/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Scrieti un program care va modela un autoturism. Un autoturism va avea o
+marca (string), un numar de inmatriculare (string), precum si o
+capacitate cilindrica(int).
+Autoturismul va avea:
+ Campurile corespunzatoare caracteristicilor autoturismului.
+ Un constructor care va initializa campurile acestuia
+ O metoda “Tiparire” care va afisa pe consola descrierea acestuia sub forma : “marca,
+numarDeInmatriculare, capacitateCilindrica”.
+Folositi: clase, campuri, metode, modificatorii de acces adecvati, comentarii
+xml pentru metodele publice.
+Creeati doua obiecte de tipul autoturism in functia “Main” si apelati-le
+metodele “Tipareste”*/
+
+CarBrand carDetailsTop = new("Dacia","BH02BH", 1200);
+CarBrand carDetailsMid = new("Mercedes", "CJ02CJ", 2000);
+
+carDetailsTop.print();
+carDetailsMid.print();
+
+class CarBrand
+{
+    private string brand;
+    private string plateNumber;
+    private int ep;
+
+    /// <summary>
+    /// Car Details:
+    /// </summary>
+    /// <param name="brand"></param>
+    /// <param name="plateNumber"></param>
+    /// <param name="ep"></param>
+    public CarBrand(string brand, string plateNumber, int ep)
+    {
+        this.brand = brand;
+        this.plateNumber = plateNumber;
+        this.ep = ep;
+    }
+    public void print()
+    {
+        Console.WriteLine(brand + " " + plateNumber + " " + ep);
+
+    }
+
+}
